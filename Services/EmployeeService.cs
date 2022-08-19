720c2cb2c6df4afacd7d7baf80cea231246b0e2b
benedictamoako <amoakobenedict@gmail.com>
2022-08-19 11:32:21 +0000

    first commit

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf_Tutorial.Models;
+
+namespace Wpf_Tutorial.Services
+{
+    public class EmployeeService
+    {
+        private static List<Employee> ObjEmployeesList;
+
+        public EmployeeService()
+        {
+
+
+            ObjEmployeesList = new List<Employee>()
+            {
+                new Employee() { Id = 101, Name = "Benedict", Age = 22},
+
+            };
+
+            // ObjEmployeesList.Add(
+        }
+
+        public List<Employee> GetAll()
+        {
+            return ObjEmployeesList;
+        }
+
+        public bool Add(Employee objNewEmployee)
+        {
+            //Age must be between 21 and 58
+            if (objNewEmployee.Age < 21 || objNewEmployee.Age > 58)
+            {
+                throw new ArgumentException("Invalid age limit for employee");
+            }
+            ObjEmployeesList.Add(objNewEmployee);
+            return true;
+        }
+
+        public bool Update(Employee objEmployeeUpdate)
+        {
+            bool IsUpdated = false;
+            for (int index = 0; index < ObjEmployeesList.Count; index++)
+            {
+                if (ObjEmployeesList[index].Id == objEmployeeUpdate.Id)
+                {
+                    ObjEmployeesList[index].Name = objEmployeeUpdate.Name;
+                    ObjEmployeesList[index].Age = objEmployeeUpdate.Age;
+                    IsUpdated = true;
+                    break;
+                }
+            }
+            return IsUpdated;
+        }
+
+        public bool Delete(int id)
+        {
+            bool IsDeleted = false;
+            for (int index = 0; index < ObjEmployeesList.Count; index++)
+            {
+                if (ObjEmployeesList[index].Id == id)
+                {
+                    ObjEmployeesList.RemoveAt(index);
+                    IsDeleted = true;
+                    break;
+                }
+            }
+            return IsDeleted;
+        }
+
+        public Employee Search(int id)
+        {
+            return ObjEmployeesList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
---
+++ ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf_Tutorial.Commands;
+using Wpf_Tutorial.Models;
+using Wpf_Tutorial.Services;
+
+namespace Wpf_Tutorial.ViewModels
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged_Implementation
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        EmployeeService ObjEmployeeService;
+
+        public EmployeeViewModel()
+        {
+            ObjEmployeeService = new EmployeeService();
+            LoadData();
+            CurrentEmployee = new Employee();
+            saveCommand = new RelayCommand(Save);
+            searchCommand = new RelayCommand(Search);
+            updateCommand = new RelayCommand(Update);
+            deleteCommand = new RelayCommand(Delete);
+        }
+
+        #region DisplayOperation
+        private ObservableCollection<Employee> employeesList;
+
+        public ObservableCollection<Employee> EmployeesList
+        {
+            get { return employeesList; }
+            set { employeesList = value; OnPropertyChanged("EmployeesList"); }
+        }
+
+        private void LoadData()
+        {
+            EmployeesList = new ObservableCollection<Employee>( ObjEmployeeService.GetAll());
+        }
+        #endregion
+
+        private Employee currentEmployee;
+
+        public Employee CurrentEmployee
+        {
+            get { return currentEmployee; }
+            set { currentEmployee = value; OnPropertyChanged("CurrentEmployee"); }
+        }
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged("Message"); }
+        }
+
+        #region SaveOperation
+        private RelayCommand saveCommand;
+
+        public RelayCommand SaveCommand
+        {
+            get { return saveCommand; }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                var isSaved = ObjEmployeeService.Add(CurrentEmployee);
+                LoadData();
+                if (isSaved)
+                {
+                    Message = "Employee Details Saved";
+                }
+                else
+                {
+                    Message = "Save Failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+        #endregion
+
+        #region SearchOperation
+        private RelayCommand searchCommand;
+
+        public RelayCommand SearchCommand
+        {
+            get { return searchCommand; }
+        }
+        
+        public void Search()
+        {
+            try
+            {
+                var ObjEmployee = ObjEmployeeService.Search(CurrentEmployee.Id);
+                if(ObjEmployee != null)
+                {
+                    CurrentEmployee.Name = ObjEmployee.Name;
+                    CurrentEmployee.Age = ObjEmployee.Age;
+                    Message = "Employee Details Below!";
+                }
+                else
+                {
+                    Message = "Employee Not Found!";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+
+            }
+        }
+        #endregion
+
+        #region UpdateOperation
+        private RelayCommand updateCommand;
+
+        public RelayCommand UpdateCommand
+        {
+            get { return updateCommand; }
+        }
+
+        public void Update()
+        {
+            try
+            {
+                var IsUpdated = ObjEmployeeService.Update(CurrentEmployee);
+                if (IsUpdated)
+                {
+                    Message = "Employee Updated!";
+                    LoadData();
+                }
+                else
+                {
+                    Message = "Update Failed!";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+        #endregion 
+
+        #region DeleteOperation
+        private RelayCommand deleteCommand;
+
+        public RelayCommand DeleteCommand
+        {
+            get { return deleteCommand; }
+        }
+
+        
+        public void Delete()
+        {
+            try
+            {
+                var IsDeleted = ObjEmployeeService.Delete(CurrentEmployee.Id);
+                if (IsDeleted)
+                {
+                    Message = "Employee Deleted";
+                    LoadData();
+                }
+                else
+                {
+                    Message = "Employee Not Found!";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+        #endregion
+    }
+}
